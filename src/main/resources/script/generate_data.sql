8336899d28ad028fa13a9a62b774c13b92b2716e
Valzavator <maxsmv1998@gmail.com>
2019-11-01 20:17:13 +0200

    init project, added sql scripts for creation db, update README.md

---
+++ src/main/resources/script/generate_data.sql
@@ -0,0 +1,14 @@
+USE periodicals;
+
+/*==============================================================*/
+/* Added data: values for presentation                          */
+/*==============================================================*/
+
+INSERT INTO users(role_id, first_name, last_name, email, password, date_of_birth, sex)
+VALUES (1, 'Maksym', 'Svynarhcuk', 'admin@gmail.com', 'admin', '1998-11-27', 'male'),
+       (2, 'Ada', 'Lovelace', 'user@gmail.com', 'user', '2000-11-13', 'female');
+
+INSERT INTO periodicals(publisher_id, frequency_id, periodical_type_id, name, price,
+                        periodical_description)
+VALUES (1, 4, 1, 'Batman', 3, 'Batman is an ongoing American comic book series featuring the DC Comics superhero Batman as its main protagonist. The character first appeared in Detective Comics #27 (cover dated May 1939). Batman proved to be so popular that a self-titled ongoing comic book series began publication with a cover date of Spring 1940. It was first advertised in early April 1940, one month after the first appearance of his new sidekick, Robin, the Boy Wonder. Batman comics have proven to be popular since the 1940s.'),
+       (2, 3, 2, 'Time', 2, 'Breaking news and analysis from TIME. Politics, world news, photos, video, tech reviews, health, science and entertainment news.');
---
+++ src/main/resources/script/mysql_script.sql
@@ -0,0 +1,280 @@
+CREATE SCHEMA IF NOT EXISTS periodicals DEFAULT CHARACTER SET utf8;
+
+USE periodicals;
+
+DROP TABLE IF EXISTS payments_subscriptions;
+
+DROP TABLE IF EXISTS payments;
+
+DROP TABLE IF EXISTS subscriptions;
+
+DROP TABLE IF EXISTS subscription_plans;
+
+DROP TABLE IF EXISTS users;
+
+DROP TABLE IF EXISTS roles;
+
+DROP TABLE IF EXISTS addresses;
+
+DROP TABLE IF EXISTS periodical_issues;
+
+DROP TABLE IF EXISTS periodicals;
+
+DROP TABLE IF EXISTS periodical_types;
+
+DROP TABLE IF EXISTS frequency;
+
+DROP TABLE IF EXISTS publishers;
+
+/*==============================================================*/
+/* Table: roles                                                 */
+/*==============================================================*/
+CREATE TABLE roles
+(
+    role_id   INT          NOT NULL AUTO_INCREMENT,
+    role_name VARCHAR(255) NOT NULL UNIQUE,
+    PRIMARY KEY (role_id)
+);
+
+/*==============================================================*/
+/* Table: addresses                                             */
+/*==============================================================*/
+CREATE TABLE addresses
+(
+    address_id     INT          NOT NULL AUTO_INCREMENT,
+    country        VARCHAR(255) NOT NULL,
+    city           VARCHAR(255) NOT NULL,
+    post_index     VARCHAR(255) NOT NULL,
+    detail_address VARCHAR(255) NOT NULL,
+    PRIMARY KEY (address_id)
+);
+
+/*==============================================================*/
+/* Table: users                                                 */
+/*==============================================================*/
+CREATE TABLE users
+(
+    user_id       INT                     NOT NULL AUTO_INCREMENT,
+    role_id       INT                     NOT NULL,
+    address_id    INT,
+    first_name    VARCHAR(255)            NOT NULL,
+    last_name     VARCHAR(255)            NOT NULL,
+    email         VARCHAR(255)            NOT NULL UNIQUE,
+    password      VARCHAR(255)            NOT NULL,
+    date_of_birth DATE                    NOT NULL,
+    sex           ENUM ('male', 'female') NOT NULL,
+    PRIMARY KEY (user_id),
+    CONSTRAINT fk_user_role
+        FOREIGN KEY (role_id) REFERENCES roles (role_id)
+            ON UPDATE CASCADE
+            ON DELETE RESTRICT,
+    CONSTRAINT fk_user_address
+        FOREIGN KEY (address_id) REFERENCES addresses (address_id)
+            ON UPDATE CASCADE
+            ON DELETE CASCADE
+);
+
+/*==============================================================*/
+/* Table: payments                                              */
+/*==============================================================*/
+CREATE TABLE payments
+(
+    payment_id   INT                                 NOT NULL AUTO_INCREMENT,
+    user_id      INT                                 NOT NULL,
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    total_price  DECIMAL(10, 2)                      NOT NULL,
+    PRIMARY KEY (payment_id),
+    CONSTRAINT fk_user_payment
+        FOREIGN KEY (user_id) REFERENCES users (user_id)
+            ON UPDATE CASCADE
+            ON DELETE RESTRICT,
+    CONSTRAINT chk_total_price CHECK ( total_price >= 0 )
+);
+
+/*==============================================================*/
+/* Table: subscription_plans                                    */
+/*==============================================================*/
+CREATE TABLE subscription_plans
+(
+    subscription_plan_id INT              NOT NULL AUTO_INCREMENT,
+    plan_name            VARCHAR(255)     NOT NULL UNIQUE,
+    months_amount        TINYINT UNSIGNED NOT NULL,
+    rate                 DECIMAL(3, 2)    NOT NULL,
+    plan_description     VARCHAR(1000),
+    PRIMARY KEY (subscription_plan_id),
+    CONSTRAINT chk_rate CHECK ( rate > 0 )
+);
+
+/*==============================================================*/
+/* Table: frequency                                             */
+/*==============================================================*/
+CREATE TABLE frequency
+(
+    frequency_id   INT          NOT NULL AUTO_INCREMENT,
+    frequency_name VARCHAR(255) NOT NULL UNIQUE,
+    meaning        VARCHAR(255) NOT NULL,
+    PRIMARY KEY (frequency_id)
+);
+
+/*==============================================================*/
+/* Table: periodical_types                                      */
+/*==============================================================*/
+CREATE TABLE periodical_types
+(
+    periodical_type_id INT          NOT NULL AUTO_INCREMENT,
+    type_name          VARCHAR(255) NOT NULL UNIQUE,
+    type_description   VARCHAR(255),
+    PRIMARY KEY (periodical_type_id)
+);
+
+/*==============================================================*/
+/* Table: publishers                                            */
+/*==============================================================*/
+CREATE TABLE publishers
+(
+    publisher_id   INT          NOT NULL AUTO_INCREMENT,
+    publisher_name VARCHAR(255) NOT NULL UNIQUE,
+    PRIMARY KEY (publisher_id)
+);
+
+/*==============================================================*/
+/* Table: periodicals                                           */
+/*==============================================================*/
+CREATE TABLE periodicals
+(
+    periodical_id          INT            NOT NULL AUTO_INCREMENT,
+    publisher_id           INT            NOT NULL,
+    frequency_id           INT            NOT NULL,
+    periodical_type_id     INT            NOT NULL,
+    name                   VARCHAR(255)   NOT NULL,
+    price                  DECIMAL(10, 2) NOT NULL,
+    periodical_description VARCHAR(1000)  NOT NULL,
+    PRIMARY KEY (periodical_id),
+    CONSTRAINT fk_periodical_type
+        FOREIGN KEY (periodical_type_id) REFERENCES periodical_types (periodical_type_id)
+            ON UPDATE CASCADE
+            ON DELETE RESTRICT,
+    CONSTRAINT fk_periodical_frequency
+        FOREIGN KEY (frequency_id) REFERENCES frequency (frequency_id)
+            ON UPDATE CASCADE
+            ON DELETE RESTRICT,
+    CONSTRAINT fk_periodical_publisher
+        FOREIGN KEY (publisher_id) REFERENCES publishers (publisher_id)
+            ON UPDATE CASCADE
+            ON DELETE RESTRICT,
+    CONSTRAINT chk_price CHECK ( price > 0 )
+);
+
+/*==============================================================*/
+/* Table: periodical_issues                                     */
+/*==============================================================*/
+CREATE TABLE periodical_issues
+(
+    periodical_issues_id INT          NOT NULL AUTO_INCREMENT,
+    periodical_id        INT          NOT NULL,
+    issues_name          VARCHAR(255) NOT NULL,
+    issue_no             INT UNSIGNED NOT NULL UNIQUE,
+    publication_date     DATE         NOT NULL,
+    issues_description   VARCHAR(1000),
+    PRIMARY KEY (periodical_issues_id),
+    CONSTRAINT fk_periodical_issue
+        FOREIGN KEY (periodical_id) REFERENCES periodicals (periodical_id)
+            ON UPDATE CASCADE
+            ON DELETE RESTRICT
+);
+
+/*==============================================================*/
+/* Table: subscriptions                                         */
+/*==============================================================*/
+CREATE TABLE subscriptions
+(
+    subscription_id      INT  NOT NULL AUTO_INCREMENT,
+    user_id              INT  NOT NULL,
+    periodical_id        INT  NOT NULL,
+    subscription_plan_id INT  NOT NULL,
+    start_date           DATE NOT NULL,
+    end_date             DATE NOT NULL,
+    PRIMARY KEY (subscription_id),
+    CONSTRAINT fk_subscription_plan
+        FOREIGN KEY (subscription_plan_id) REFERENCES subscription_plans (subscription_plan_id)
+            ON UPDATE CASCADE
+            ON DELETE RESTRICT,
+    CONSTRAINT fk_subscription_periodical
+        FOREIGN KEY (periodical_id) REFERENCES periodicals (periodical_id)
+            ON UPDATE CASCADE
+            ON DELETE RESTRICT,
+    CONSTRAINT fk_user_subscription
+        FOREIGN KEY (user_id) REFERENCES users (user_id)
+            ON UPDATE CASCADE
+            ON DELETE RESTRICT,
+    CONSTRAINT chk_start_end_time CHECK ( end_date > start_date )
+);
+
+/*==============================================================*/
+/* Table: payments_subscriptions                                */
+/*==============================================================*/
+CREATE TABLE payments_subscriptions
+(
+    subscription_id INT NOT NULL,
+    payment_id      INT NOT NULL,
+    PRIMARY KEY (subscription_id, payment_id),
+    CONSTRAINT fk_payments_subscriptions FOREIGN KEY (payment_id)
+        REFERENCES payments (payment_id) ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+    CONSTRAINT fk_subscriptions_payments
+        FOREIGN KEY (subscription_id) REFERENCES subscriptions (subscription_id)
+            ON UPDATE CASCADE
+            ON DELETE RESTRICT
+);
+
+/*==============================================================*/
+/* Added default values                                         */
+/*==============================================================*/
+
+INSERT INTO roles(role_name)
+VALUES ('admin'),
+       ('user');
+
+INSERT INTO subscription_plans(plan_name, months_amount, rate)
+VALUES ('1 month', 1, 1),
+       ('3 month', 3, 0.9),
+       ('6 month', 6, 0.8),
+       ('12 month', 12, 0.7);
+
+INSERT INTO periodical_types(type_name, type_description)
+VALUES ('Comics', ''),
+       ('Magazine', ''),
+       ('Newspaper', ''),
+       ('Manga', ''),
+       ('Others', '');
+
+INSERT INTO frequency(frequency_name, meaning)
+VALUES ('Daily', 'Once per business day'),
+       ('Semi-weekly', 'Twice per week'),
+       ('Weekly', 'Every week'),
+       ('Biweekly, Fortnightly', 'Every two weeks'),
+       ('Semi-monthly', 'Twice per month'),
+       ('Monthly', 'Every month'),
+       ('Semi-quarterly', 'Twice per quarter'),
+       ('Bimonthly', 'Every 2 months'),
+       ('Quarterly', 'Every quarter'),
+       ('Triannually', 'Thrice per year'),
+       ('Semiannually, Biannually', 'Twice per year'),
+       ('Annually', 'Once per year'),
+       ('Biennially', 'Once per 2 years'),
+       ('Triennially', 'Once per 3 years'),
+       ('Quadriennially', 'Once per 4 years'),
+       ('Quinquennially', 'Once per 5 years');
+
+
+INSERT INTO publishers(publisher_name)
+VALUES ('Pearson'),
+       ('ThomsonReuters'),
+       ('Penguin Random House'),
+       ('RELX Group'),
+       ('Hachette Livre'),
+       ('Grupo Planeta'),
+       ('Wiley'),
+       ('OLMA Media Group'),
+       ('Klett'),
+       ('Wolters Kluwer');
